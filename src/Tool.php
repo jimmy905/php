7d8792879f00c9ce94dd61285b4070e873a9421f
jimmy905 <136180896@qq.com>
2021-11-24 21:35:07 +0800

    Update Tool.php

--- src/Tool.php
+++ src/Tool.php
@@ -2,6 +2,9 @@
 
 namespace yuanhou\composer;
 
+use think\facade\Db;
+use think\Request;
+
 class Tool
 {
     public function index()
@@ -12,5 +15,24 @@ class Tool
     static function test()
     {
         echo 'this is tool test';
+    }
+
+    // 小数点后保留两位
+    static function xiaoshu($num, $wei = 2)
+    {
+        return  number_format($num, $wei, ".", "");
+    }
+
+
+    // 获取会员
+    static function gethuiyuan(Request $request)
+    {
+
+        $openid = $request->param('token');
+
+        // 查询会员
+        $huiyuan = Db::name("yuanhou_user")->where('token', $openid)->find();
+
+        return $huiyuan;
     }
 }
