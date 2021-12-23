bc647332532f7e2279719147a793bc22f99aa150
jimmy905 <136180896@qq.com>
2021-12-23 14:39:29 +0800

    Update Tool.php

--- src/Tool.php
+++ src/Tool.php
@@ -270,10 +270,29 @@ class Tool
 
         $url = " https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" . $appid . "&secret=" . $secret;
 
-        $neirong = file_get_contents($url);
+        $neirong = self::curl_get_https($url);
 
 
         var_dump($neirong);
+    }
+
+
+
+
+
+    // httpsget请求
+    static function curl_get_https($url)
+    {
+        $curl = curl_init(); // 启动一个CURL会话
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);  // 从证书中检查SSL加密算法是否存在
+        $tmpInfo = curl_exec($curl);     //返回api的json对象
+        //关闭URL请求
+        curl_close($curl);
+        return $tmpInfo;    //返回json对象
     }
 
 
