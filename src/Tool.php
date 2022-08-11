3d52b33f70aae2504b5af3fa27c366c6b5b469d1
jimmy905 <136180896@qq.com>
2022-08-11 14:27:51 +0800

    11
    11

--- src/Tool.php
+++ src/Tool.php
@@ -1500,4 +1500,39 @@ class Tool
             if ($var['pId'] == $val['pId']) return true;
         });
     }
+
+
+
+    static public function ClearHtml($str)
+
+    {
+
+        $str = trim($str); //清除字符串两边的空格
+
+        $str = strip_tags($str, ""); //利用php自带的函数清除html格式
+
+        $str = preg_replace("/\t/", "", $str); //使用正则表达式替换内容，如：空格，换行，并将替换为空。
+
+        $str = preg_replace("/\r\n/", "", $str);
+
+        $str = preg_replace("/\r/", "", $str);
+
+        $str = preg_replace("/\n/", "", $str);
+
+        $str = preg_replace("/ /", "", $str);
+
+        $str = preg_replace("/ /", "", $str); //匹配html中的空格
+
+        return trim($str); //返回字符串
+
+    }
+
+    static public function miaoshu($str, $num)
+    {
+        $neinei = self::ClearHtml($str);
+        $neinei = htmlspecialchars($neinei);
+        $miaoshu = mb_substr($neinei, 0, $num, 'UTF-8');
+
+        return $miaoshu;
+    }
 }
