67a16501fb4a04f9fe6e39b46cf5fc85f9b7769d
jimmy905 <136180896@qq.com>
2023-02-07 12:59:20 +0800

    Update Tool.php
    12

--- src/Tool.php
+++ src/Tool.php
@@ -37,6 +37,10 @@ class Tool
         return true;
     }
 
+
+
+
+
     // redis取
     static function redisqu($key)
     {
@@ -45,6 +49,70 @@ class Tool
         $test = Cache::store('redis')->get($biaoshi);
 
         return $test;
+    }
+
+
+    // 验证手机号码
+    static function checkPhone($mobile)
+    {
+        if (!preg_match("/^1[3456789]{1}\d{9}$/", $mobile)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    // 验证身份证是否正确
+    static function checkShenfenzheng($num_id)
+    {
+
+        $num_id = strtoupper($num_id);
+        $regx = "/^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$|^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/";
+        $arr_split = array();
+        if (!preg_match($regx, $num_id)) {
+            return FALSE; //正则校验
+        }
+        if (15 == strlen($num_id)) //检查15位
+        {
+            $regx = "/^(\d{6})+(\d{2})+(\d{2})+(\d{2})+(\d{3})$/";
+            @preg_match($regx, $num_id, $arr_split);
+            //检查生日日期是否正确
+            $dtm_birth = "19" . $arr_split[2] . '/' . $arr_split[3] . '/' . $arr_split[4];
+            if (!strtotime($dtm_birth)) {
+                return FALSE;
+            } else {
+                return TRUE;
+            }
+        } else      //检查18位
+        {
+            $regx = "/^(\d{6})+(\d{4})+(\d{2})+(\d{2})+(\d{3})([0-9]|X)$/";
+            @preg_match($regx, $num_id, $arr_split);
+            $dtm_birth = $arr_split[2] . '/' . $arr_split[3] . '/' . $arr_split[4];
+            if (!strtotime($dtm_birth)) //检查生日日期是否正确
+            {
+                return FALSE;
+            } else {
+                //检验18位身份证的校验码是否正确。
+                //校验位按照ISO 7064:1983.MOD 11-2的规定生成，X可以认为是数字10。
+                $arr_int = array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2);
+                $arr_ch = array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2');
+                $sign = 0;
+                for ($i = 0; $i < 17; $i++) {
+                    $b = (int) $num_id{
+                        $i};
+                    $w = $arr_int[$i];
+                    $sign += $b * $w;
+                }
+                $n = $sign % 11;
+                $val_num = $arr_ch[$n];
+                if ($val_num != substr($num_id, 17, 1)) {
+                    return FALSE;
+                } else {
+                    return TRUE;
+                }
+            }
+        }
     }
 
 
@@ -382,6 +450,8 @@ class Tool
         return     $zuotian;
     }
 
+
+
     // 获取日期
     static  function riqi($date = null, $tian)
     {
@@ -415,6 +485,10 @@ class Tool
 
         return     $shangyue;
     }
+
+
+
+
 
 
     // 获取上月昨天
@@ -1630,6 +1704,9 @@ class Tool
                 $pics = $data['pics'];
                 $data['pics'] = json_encode($pics);
             }
+
+
+
 
 
 
